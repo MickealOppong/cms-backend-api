a82926a60d0b75436c68b9415d538640e6d79df0
yaw oppong <opp.mic@gmail.com>
2024-07-14 10:36:56 +0200

    config

--- src/main/java/opp/mic/cms/config/SecurityConfig.java
+++ src/main/java/opp/mic/cms/config/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("https://e-cms.netlify.app"));
         config.setAllowedMethods(Arrays.asList("POST","GET","DELETE","PUT","PATCH","OPTIONS","HEAD"));
         config.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers","Access-Control-Allow-Origin","Access-Control-Request-Method", "Access-Control-Request-Headers","Origin","Cache-Control", "Content-Type", "Authorization"));
         config.setAllowCredentials(true);
