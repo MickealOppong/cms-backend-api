eff28c44d2eeeaa432baf6ae15d67646652c32ce
yaw oppong <opp.mic@gmail.com>
2024-07-14 10:29:49 +0200

    config

--- src/main/java/opp/mic/cms/config/SecurityConfig.java
+++ src/main/java/opp/mic/cms/config/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://estore-cms.vercel.app"));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(Arrays.asList("POST","GET","DELETE","PUT","PATCH","OPTIONS","HEAD"));
         config.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers","Access-Control-Allow-Origin","Access-Control-Request-Method", "Access-Control-Request-Headers","Origin","Cache-Control", "Content-Type", "Authorization"));
         config.setAllowCredentials(true);
